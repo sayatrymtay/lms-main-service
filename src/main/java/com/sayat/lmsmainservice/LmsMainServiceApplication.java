d398b882f2c7143f0c7cc9431946440c12166b29
Sayat <saiat.rymtai06@gmail.com>
2026-05-30 01:46:53 +0500

    Initial Spring Boot project setup

---
+++ src/main/java/com/sayat/lmsmainservice/LmsMainServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sayat.lmsmainservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsMainServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsMainServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sayat/lmsmainservice/LmsMainServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sayat.lmsmainservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsMainServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
